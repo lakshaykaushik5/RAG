f79ebcd1a60fa95e2ef9a14d124d509db5b06b99
lakshaykaushik051 <lakshaykaushik051@gmail.com>
2025-05-07 05:47:55 +0000

    human_in_the_loop

--- langgraph/graph_2_with_memory.py
+++ langgraph/graph_2_with_memory.py
@@ -5,8 +5,9 @@ from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import ToolNode , tools_condition
 from langchain_google_genai import ChatGoogleGenerativeAI
-
+from langgraph.types import Command, interrupt
 from dotenv import load_dotenv
+from langchain_core.tools import tool
 import os
 
 
@@ -21,18 +22,42 @@ memory = MemorySaver()
 
 llm = ChatGoogleGenerativeAI(model=os.getenv('CHAT_MODEL_NAME'),google_api_key=os.getenv('GEMINI_API_KEY'))
 
+
+
+
+@tool
+def human_assistance(query:str)->str:
+    """Request assistance from human ."""
+    human_response = interrupt({'query':query})
+    return human_response['data']
+
+
+tools = [human_assistance]
+
+
+llm_with_tools = llm.bind_tools(tools)
+tool_node = ToolNode(tools=tools)
+
 def chatbot(state:State):
-    return {'messages':[llm.invoke(state['messages'])]}
+    return {'messages':[llm_with_tools.invoke(state['messages'])]}
 
 
 graph_builder = StateGraph(State)
 
 graph_builder.add_node('chatbot',chatbot)
 
-graph_builder.add_edge(START,'chatbot')
-graph_builder.add_edge('chatbot',END)
+graph_builder.add_node('tools',tool_node)
 
-config = {'configurable':{"thread_id":"1"}}
+
+
+graph_builder.add_edge(START,'chatbot')
+graph_builder.add_conditional_edges(
+    'chatbot',
+    tools_condition
+)
+# graph_builder.add_edge('chatbot',END)
+
+config = {'configurable':{"thread_id":"2"}}
 
 graph = graph_builder.compile(checkpointer = memory)
 
@@ -45,6 +70,26 @@ def call_llm():
 
         events =  graph.stream(user_input,config,stream_mode='values')
         for value in events:
-            value['messages'][-1].pretty_print()
+            if 'messages' in value:
+                value['messages'][-1].pretty_print()
+            else:
+                snapshot = graph.get_state(config)
+                print(snapshot.next)
+                human_response=(
+                    "No one can meet admin directly, only few people can meet to admin"
+                )
+
+                # call_llm()
+                human_command = Command(resume={'data':human_response})
+                events = graph.stream(human_command,config,stream_mode='values')
+                for event in events:
+                    if 'messages' in event:
+                        event['messages'][-1].pretty_print()
+
+
 
 call_llm()
+    
+    
+
+
